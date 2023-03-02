be99fc65d79e48de8ed48bdd074cdf378b55fd9f
Jahanzaib <mianjuzzi@gmail.com>
2023-03-02 12:54:47 +0500

    add

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+title: 'ahtisham ',
+debugShowCheckedModeBanner: false,
+home: Home(),
+    );
+  }
+}
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar:AppBar(
+      backgroundColor: Colors.amber,
+      leading: const Icon(
+       Icons.menu,
+       color:Colors.white,
+      ),
+      title:const Text('Jahanzaib'),
+      
+      actions: const [
+Icon(
+  Icons.access_time,
+  color: Colors.white,
+),
+Icon(
+  Icons.search
+),
+Icon(
+  Icons.more_vert,
+  color: Colors.white,
+)
+
+
+      ],
+      ),
+  
+      );
+    
+   }
+}
